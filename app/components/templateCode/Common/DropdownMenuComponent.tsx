2491d68b3790b25426e84f621cb488a9515c1740
Abrar Ahmed <99709358+AbrarAhmed111@users.noreply.github.com>
2024-05-29 07:47:50 +0500

    Update DropdownMenuComponent.tsx

--- app/components/templateCode/Common/DropdownMenuComponent.tsx
+++ app/components/templateCode/Common/DropdownMenuComponent.tsx
@@ -6,42 +6,22 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import Link from "next/link";
-import { useEffect } from "react";
+import { signOut } from "next-auth/react";
 
 export function DropdownMenuComponent({ open, setOpen }) {
-
-  const closeDropdown = () => {
-    setOpen(false);
-  };
-
-  useEffect(() => {
-    const handleOutsideClick = (event) => {
-      if (!event.target.closest(".dropdown-container")) {
-        closeDropdown();
-      }
-    };
-
-    if (open) {
-      document.addEventListener("click", handleOutsideClick);
-    } else {
-      document.removeEventListener("click", handleOutsideClick);
-    }
-
-    return () => {
-      document.removeEventListener("click", handleOutsideClick);
-    };
-  }, [open]);
   return (
     <DropdownMenu open={open} onOpenChange={setOpen}>
       <DropdownMenuTrigger>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-[240px] p-2 mx-4 mt-8">
         <DropdownMenuItem className="font-semibold text-lg hover:bg-zinc-100 pt-3">
-          <Link href="/resume" >
+          <Link href="/dashboard" >
             Dashboard
           </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem className="font-semibold text-lg hover:cursor-pointer hover:bg-zinc-100">
+        <DropdownMenuItem  onClick={() => {
+                        signOut();
+                      }} className="font-semibold text-lg hover:cursor-pointer hover:bg-zinc-100">
           Logout
         </DropdownMenuItem>
 
